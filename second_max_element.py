2bdf03ea7bf00ab90a92bf742d85af40dcd8b57f
sidshukla74 <108859004+sidshukla74@users.noreply.github.com>
2022-07-09 13:46:31 +0530

    Add files via upload

---
+++ second_max_element.py
@@ -0,0 +1,11 @@
+#sort the array and print second largest element
+
+a = [34,23,11,10,4,5,3,31,56]
+
+n=len(a)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if a[j]>a[j+1]:
+            a[j],a[j+1] = a[j+1], a[j]
+
+print("the second largest element in the list is=",a[-2])
